d981d5b93781d8714fb34dafbfbeb99864ba35ed
PNB <pablobeccalli@gmail.com>
2020-10-16 01:57:48 -0300

    V 1.0.0
    Initial Design

---
+++ js/parallax.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(e) {
+  parallax();
+})
+function parallax() {
+  var scroll = $(window).scrollTop();
+  var screenHeight = $(window).height();
+
+  $('.parallax').each(function() {
+    var offset = $(this).offset().top;
+    var distanceFromBottom = offset - scroll - screenHeight
+
+    if (offset > screenHeight && offset) {
+      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.35) +'px');
+    } else {
+      $(this).css('background-position', 'center ' + (( -scroll ) * 0.35) + 'px');
+    }
+  })
+}
